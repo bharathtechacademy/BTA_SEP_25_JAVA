7845e0d791defec56e57b058e950f0412831da4d
bharathautomation <soft.ittech20@gmail.com>
2025-09-26 21:19:20 +0530

    Assignment 1 - Data Types

---
+++ src/language/basics/DataTypes.java
@@ -0,0 +1,18 @@
+package language.basics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		// 1.Temperature of a city in degrees Celsius: 25.5
+		float temperature = 25.5f;
+		System.out.println("Temperature of City : "+temperature);
+		
+		// 2.Whether a customer has placed an order: true or false
+		boolean orderPlaced = true;
+		System.out.println("Order Status :"+orderPlaced);
+
+
+	}
+
+}
