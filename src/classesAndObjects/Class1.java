332dcb5f0a668fc1e7027751a093a2e575e480fe
bharathautomation <soft.ittech20@gmail.com>
2025-09-30 21:26:20 +0530

    Classes and Objects

---
+++ src/classesAndObjects/Class1.java
@@ -0,0 +1,14 @@
+package classesAndObjects;
+
+public class Class1 {
+
+	int empId = 1234;
+	static String empName = "Bharath";
+
+	public static void main(String[] args) {
+		Class1 obj =  new Class1();	
+		System.out.println(obj.empId);
+		System.out.println(empName);
+	}
+
+}
---
+++ src/collections/CollectionsInJava.java
@@ -0,0 +1,26 @@
+package collections;
+
+public class CollectionsInJava {
+	
+	
+
+	public static void main(String[] args) {
+		
+		/***** Array  *****/	
+		// Array is a data type , that can store multiple values with similar data type together at one place		
+
+		String [] empNames = new String[3]; // This can hold 3 employee names.
+		empNames[0] = "Bharath";
+		empNames[1] = "ABC";
+		empNames[2] = "XYZ";	
+		System.out.println(empNames.length);	
+		
+		/**** Challenges with Arrays ****/
+		// 1. Arrays are fixed in size
+		
+		
+		
+
+	}
+
+}
