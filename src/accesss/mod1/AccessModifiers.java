2d9971f0f4d1fd95901620aa8eb2b8edd2833de5
bharathautomation <soft.ittech20@gmail.com>
2025-10-13 21:31:33 +0530

    changes

--- src/accesss/mod1/AccessModifiers.java
+++ src/accesss/mod1/AccessModifiers.java
@@ -6,9 +6,10 @@ public class AccessModifiers {
 	
 	//4 types of Access Modifiers in Java:
 	
-	//1. public (Accessible from anywhere)
-	//2. protected (Accessible within the same package and child classes in other packages)
-	//3. default (no modifier) (Accessible only within the same package)
-	//4. private (Accessible only within the same class)
-
+	//1. public (Accessible from anywhere-with in the class, package, out side of package) ==> class, method and variable
+	//2. protected (Accessible within the same package and child classes in other packages) ==> method and variable
+	//3. default (no modifier) (Accessible only within the same package) ==> class, method and variable
+	//4. private (Accessible only within the same class)==> method and variable
+	
+	
 }
